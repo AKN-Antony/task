b53ae0a07f0f39bfcc5d1752fa19c64ca2d92fce
AKN-Antony <kamauanthony345@gmail.com>
2026-09-10 22:43:24 +0300

    Add comments on task

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Comment
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'priority', 'assignee', 'organization', 'created_at')
     list_filter = ('status', 'priority', 'is_deleted')
     search_fields = ('title', 'description')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'task', 'created_at')
+    search_fields = ('content', 'author__email', 'task__title')
