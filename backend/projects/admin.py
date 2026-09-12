693c391edbe9283d5fe5d22884969f63fd543593
AKN-Antony <kamauanthony345@gmail.com>
2026-09-12 14:41:59 +0300

    add project model and linnk tasks to projects

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import Project
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'organization', 'is_active', 'start_date', 'end_date')
+    search_fields = ('name', 'description')
+    list_filter = ('is_active', 'organization')
