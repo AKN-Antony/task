dc2872a746be55cb21a65815e8faf9c4d2346c77
AKN-Antony <kamauanthony345@gmail.com>
2026-09-12 14:41:44 +0300

    add project model and linnk tasks to projects

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,1 +1,23 @@
+import uuid
 from django.db import models
+from django.conf import settings
+from accounts.models import Organization
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='owned_projects', on_delete=models.CASCADE)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='projects', blank=True)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='projects')
+    
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
