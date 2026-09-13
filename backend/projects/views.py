9040f66fd67c92d1ef592330e8398ee2529e265e
AKN-Antony <kamauanthony345@gmail.com>
2026-09-13 22:38:17 +0300

    Create API endpoints for projects milestones and sprints

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,1 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project, Milestone, Sprint
+from .serializers import ProjectSerializer, MilestoneSerializer, SprintSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.filter(organization=self.request.user.organization)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            owner=self.request.user,
+            organization=self.request.user.organization
+        )
+
+class MilestoneViewSet(viewsets.ModelViewSet):
+    serializer_class = MilestoneSerializer
+
+    def get_queryset(self):
+        return Milestone.objects.filter(project__organization=self.request.user.organization)
+
+class SprintViewSet(viewsets.ModelViewSet):
+    serializer_class = SprintSerializer
+
+    def get_queryset(self):
+        return Sprint.objects.filter(project__organization=self.request.user.organization)
