e660e815e94fc784ba08c9219a3b8343724237f4
AKN-Antony <kamauanthony345@gmail.com>
2026-09-13 22:32:07 +0300

    Implement comment and attachments endpoints

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, TagViewSet
+from .views import TaskViewSet, TagViewSet, CommentViewSet, AttachmentViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 router.register(r'tags', TagViewSet, basename='tag')
+router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'attachments', AttachmentViewSet, basename='attachment')
 
 urlpatterns = [
     path('', include(router.urls)),
