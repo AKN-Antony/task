98f6e3b396ebcd33732cfcd22d7fcc40b3c7647e
AKN-Antony <kamauanthony345@gmail.com>
2026-09-13 22:40:58 +0300

    Implement workflow engine with custom stages and transition rules

--- backend/workflows/views.py
+++ backend/workflows/views.py
@@ -1,1 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Workflow, WorkflowStage, TransitionRule
+from .serializers import WorkflowSerializer, WorkflowStageSerializer, TransitionRuleSerializer
+
+class WorkflowViewSet(viewsets.ModelViewSet):
+    serializer_class = WorkflowSerializer
+
+    def get_queryset(self):
+        return Workflow.objects.filter(organization=self.request.user.organization)
+
+    def perform_create(self, serializer):
+        serializer.save(organization=self.request.user.organization)
+
+class WorkflowStageViewSet(viewsets.ModelViewSet):
+    serializer_class = WorkflowStageSerializer
+
+    def get_queryset(self):
+        return WorkflowStage.objects.filter(workflow__organization=self.request.user.organization)
+
+class TransitionRuleViewSet(viewsets.ModelViewSet):
+    serializer_class = TransitionRuleSerializer
+
+    def get_queryset(self):
+        return TransitionRule.objects.filter(workflow__organization=self.request.user.organization)
